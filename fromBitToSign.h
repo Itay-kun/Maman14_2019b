372422494ef290365724bb65b932b13afb14a88c
amitginat23 <amitginat23@gmail.com>
2019-07-16 15:22:47 +0300

    ftom bits to 4th base

---
+++ fromBitToSign.h
@@ -0,0 +1,5 @@
+#define numOfActiveBites 							14 /* should be int he main.h!!!!!!!!!!!!!!*/
+#define maxNumDecimalAddress 						1000
+
+int 	from_binary_machine_code_to_fourth_base 	(short *);
+int 	to_4_base									(char, char, file *);
