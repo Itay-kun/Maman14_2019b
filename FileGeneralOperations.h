e0d33d70dbe6941a6be84caa770858eac3faf617
avivka <avivka8@gmail.com>
2019-07-16 15:28:13 +0300

    FileGeneralOperations.h:Fix typo in the includes part

--- FileGeneralOperations.h
+++ FileGeneralOperations.h
@@ -5,7 +5,7 @@
 #ifndef MAMAN14_2019B_FILEGENERALOPERATIONS_H
 #define MAMAN14_2019B_FILEGENERALOPERATIONS_H
 
-#include <stdio.c>
-#include <stdlib.c>
+#include <stdio.h>
+#include <stdlib.h>
 
-#endif //MAMAN14_2019B_FILEGENERALOPERATIONS_H
+#endif MAMAN14_2019B_FILEGENERALOPERATIONS_H
